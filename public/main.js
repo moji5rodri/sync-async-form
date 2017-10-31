bae901e807c2340147043d57eb6ae1f903742463
Alumno10 J601 <a10j601@campustls.local>
2017-10-31 12:53:24 -0500

    agrego

---
+++ public/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	$('input[type="submit"]').on("click", function(event){
+		var first_name = $('input[name="first_name"]').val();
+		console.log(first_name);
+
+		event.preventDefault();
+	});
+});
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app   = express();
-const path  = require("path");
-​
+const app     = express();
+const path    = require("path");
+
 app.use("/", express.static("public"));
-​
+
 app.listen(8080, function () {
- console.log('Listening on port 8080!')
-})
+  console.log('Listening on port 8080!')
+});
