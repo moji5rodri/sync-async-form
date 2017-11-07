05457f84c73887d9f3cd93eebd989b901a8e1bed
Alumno01 J601 <a01j601@campustls.local>
2017-11-07 12:48:41 -0500

    agrego

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-
 	$('input[type="submit"]').on("click", function(event){
-		var first_name = $('input[name="first_name"]').val();
-		console.log(first_name);
-
 		event.preventDefault();
+		
+		var first_name = $('#first_name').val();
+
+		$.get('find.html', {'first_name': first_name});
 	});
 });
