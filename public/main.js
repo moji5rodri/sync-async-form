d5eed2754474859b4cd6a847f31d2699b3f81fc1
Alumno07 J901 <a07j901@campustls.local>
2017-11-14 09:36:38 -0500

    agregar

--- public/main.js
+++ public/main.js
@@ -1,9 +1,34 @@
-$(document).ready(function () {
-	$('input[type="submit"]').on("click", function(event){
-		event.preventDefault();
-		
-		var first_name = $('#first_name').val();
 
-		$.get('find.html', {'first_name': first_name});
-	});
+$(document).ready(function () {
+ //console.log("estoy listo");
+ //alert("Hola");
+
+  $('input[type="submit"]').on("click", function(event){
+    event.preventDefault();
+
+    var first_name = $('#first_name').val();
+
+    $.ajax('find.html', {
+      method: 'post',
+      data: {
+        'first_name': first_name
+      }
+
+    });
+
+    var full_name = $('#full_name').val();
+    /*--version abreviada--*/
+     $.post('find.html', {'full_name' : full_name});
+
+    var gender = $('input[name="gender"]:checked').val();
+    $.post('find.html', {'gender' : gender});
+
+    var interest = $('input[name="interest"]:checked').val();
+    $.post('find.html', {'interest' : interest});
+
+
+
+  });
+
+
 });
