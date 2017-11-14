4995f568f67c0b5bf76e2f2cbc2a80f3180ae8ee
moji5rodri <31252698+moji5rodri@users.noreply.github.com>
2017-11-14 13:30:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.post("/find", (request, response) => {
+      console.log(request.body.first_name),
+      console.log(request.body.last_name);
+
+      response.json({"method": "post", "success": true});
+});
 app.get("/find", (request, response) => {
-    console.log(request.query.first_name);
-    console.log(request.query.full_name);
-    
-    response.json({"method": "get", "success": true});
-    
+      console.log(request.query.first_name),
+      console.log(request.query.last_name);
+
+      response.json({"method": "get", "success": true});
 });
 
 app.use("/", express.static("public"));
